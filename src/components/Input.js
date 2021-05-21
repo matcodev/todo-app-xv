8506905956174ed5210f9e4188e2b88b8ee228be
mxespin <matias@clippercapital.cl>
2021-05-20 22:10:36 -0400

    fix

--- src/components/Input.js
+++ src/components/Input.js
@@ -9,32 +9,41 @@ const InputToDo = (props) => {
         const { name, value } = e.target
         setTask({...task, [name]:value})
     }
-    const sendTask = (e)=> {
-        e.preventDefault()
-        const noValid = e.target.name.length <= 0 ? true : false;
-        console.log(noValid)
-        if(task.task === true){
+    const onKeyUpValue = (e) => {
+       
+        if(e.key === 'Enter'){
+         console.log(e.target.value)
+         if(task.task.split(' ').join('') !== ''){
             props.addToDo(task)
             setTask(initialToDo)
+        } 
         }
+    }
+
+    const sendTask = (e)=> {
+        e.preventDefault()
+        //const noValid = e.target.value.length !== '' ? true : false;
+         // if(task.task.replace(/ /g,"") !== ''){
+                // console.log(task.task)
     }
     return (
         <div className="row">
             <div className="text-center py-5">
                 <h1>To Do App</h1>
             </div>
-            <form >
+            <form onSubmit={sendTask}>
             <input 
                 className="form-control form-control-lg" 
                 type="text" 
                 placeholder="Any homework to do?"
                 name="task"
                 onChange={handlePressKey}
+                onKeyUp={onKeyUpValue.bind(this)}
                 value= {task.task}
                 />
-                <button className="btn btn-primary mt-3" onClick={sendTask}>
+                {/* <button className="btn btn-primary mt-3">
                     Create
-                </button>
+                </button> */}
             </form>    
         </div>
     );
